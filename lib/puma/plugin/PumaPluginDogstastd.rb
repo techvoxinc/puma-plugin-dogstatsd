48182bfa093332cf8a297f1d383a1ab5775677fd
Jules Ivanic <guizmaii@users.noreply.github.com>
2019-03-28 17:29:41 +0100

    Use the batch capability of Dogstatsd && Improve code (#1)
    * Add some debug and replace `gauge` by `count`
    
    * Clean
    
    * Use the batch capability of Dogstashd && increment a 'puma.stats.sent' key
    
    * Clean
    
    * Replace `count` by `gauge`

--- lib/puma/plugin/PumaPluginDogstastd.rb
+++ lib/puma/plugin/PumaPluginDogstastd.rb
@@ -27,23 +27,27 @@ Puma::Plugin.create do
 
     clustered = launcher.send(:clustered?) # See https://github.com/puma/puma/blob/master/lib/puma/launcher.rb#L285
 
-    launcher.events.debug "PumaPluginDatadogStastd: enabled. Cluster mode: #{clustered}"
+    launcher.events.debug "PumaPluginDatadogStastd - enabled. Cluster mode: #{clustered}"
 
     in_background do
       sleep 5
       loop do
-        launcher.events.debug 'PumaPluginDatadogStastd: notify statsd'
         begin
-          stats = JSON.parse(Puma.stats)
+          stats = Puma.stats
+          launcher.events.debug "PumaPluginDatadogStastd - notify stats: #{stats}"
 
-          dogstatsd_client.gauge('puma.workers', stats.fetch('workers', 1))
-          dogstatsd_client.gauge('puma.booted_workers', stats.fetch('booted_workers', 1))
-          dogstatsd_client.gauge('puma.running', count_value_for_key(clustered, stats, 'running'))
-          dogstatsd_client.gauge('puma.backlog', count_value_for_key(clustered, stats, 'backlog'))
-          dogstatsd_client.gauge('puma.pool_capacity', count_value_for_key(clustered, stats, 'pool_capacity'))
-          dogstatsd_client.gauge('puma.max_threads', count_value_for_key(clustered, stats, 'max_threads'))
+          parsed_stats = JSON.parse(stats)
+
+          dogstatsd_client.batch do |s|
+            s.gauge('puma.workers', parsed_stats.fetch('workers', 1))
+            s.gauge('puma.booted_workers', parsed_stats.fetch('booted_workers', 1))
+            s.gauge('puma.running', count_value_for_key(clustered, parsed_stats, 'running'))
+            s.gauge('puma.backlog', count_value_for_key(clustered, parsed_stats, 'backlog'))
+            s.gauge('puma.pool_capacity', count_value_for_key(clustered, parsed_stats, 'pool_capacity'))
+            s.gauge('puma.max_threads', count_value_for_key(clustered, parsed_stats, 'max_threads'))
+          end
         rescue StandardError => e
-          launcher.events.error "PumaPluginDatadogStastd: notify stats failed:\n  #{e.to_s}\n  #{e.backtrace.join("\n    ")}"
+          launcher.events.error "PumaPluginDatadogStastd - notify stats failed:\n  #{e.to_s}\n  #{e.backtrace.join("\n    ")}"
         ensure
           sleep 2
         end
